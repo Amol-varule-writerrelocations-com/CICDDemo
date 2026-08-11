9012730bbaadcf453d86a95516f65214ed35aae6
Amol Varule <amol.varule@writerrelocations.com>
2026-08-11 10:23:55 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CICDDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace CICDDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var employee = new Employee
+            {
+                Id = 1,
+                Name = "Amol",
+                Department = "IT"
+            };
+
+            return View(employee);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
